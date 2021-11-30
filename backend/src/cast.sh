8567e62d30bfe117d0bd64671737b2b9fbfe0d1c
Kafonek <matt.kafonek@noteable.io>
2021-11-30 11:51:54 -0500

    transition to using poetry

--- backend/src/cast.sh
+++ backend/src/cast.sh
@@ -2,6 +2,6 @@ set -e
 
 wait-for cockroach:26257 -- echo "Cockroach is up"
 
-alembic upgrade head 
+poetry run alembic upgrade head 
 
-python -m debugpy --listen 0.0.0.0:5678 -m uvicorn app.main:app --reload --host 0.0.0.0
+poetry run python -m debugpy --listen 0.0.0.0:5678 -m uvicorn app.main:app --reload --host 0.0.0.0
