de76a4fbb46bc61097c733f94a1d4165e44b6a80
Kafonek <matt.kafonek@noteable.io>
2021-12-05 16:09:56 -0500

    support running behind reverse proxy in subdirectory

--- backend/src/app/main.py
+++ backend/src/app/main.py
@@ -1,3 +1,5 @@
+import os
+
 from fastapi import Depends, FastAPI
 
 from app.auth import get_user
@@ -6,9 +8,10 @@ from app.crud import router as CrudRouter
 from app.log_utils import setup_logging
 from app.models import UserDAO
 from app.schemas import UserOut
+from app.settings import get_settings
 
 setup_logging()
-app = FastAPI()
+app = FastAPI(root_path=get_settings().ROOT_PATH)
 app.include_router(AuthRouter)
 app.include_router(CrudRouter)
 
@@ -16,3 +19,10 @@ app.include_router(CrudRouter)
 @app.get("/me", response_model=UserOut)
 def me(user: UserDAO = Depends(get_user)):
     return user
+
+
+# Useful for seeing which backend your browser is connected to
+# when multiple backends are running behind load-balancer.
+@app.get("/host")
+def environ():
+    return os.environ.get("HOSTNAME")
