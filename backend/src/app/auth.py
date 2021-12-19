078ba532ba8f6af1996ab4f62f067e3e51dbff8d
Kafonek <matt.kafonek@noteable.io>
2021-12-19 13:36:06 -0500

    put JWT in auth header instead of simple username string

--- backend/src/app/auth.py
+++ backend/src/app/auth.py
@@ -1,28 +1,54 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jose import JWTError, jwt
 
 from app.db import db_session
 from app.models import UserDAO
+from app.settings import get_settings
 
 router = APIRouter(prefix="/auth")
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
 
+SECRET_KEY = get_settings().SECRET_KEY
+ALGORITHM = "HS256"
+
 
 @router.post("/login", include_in_schema=False)
 async def login(form_data: OAuth2PasswordRequestForm = Depends()):
+    "Authenticate a user and return a JWT token used for further requests"
     async with db_session() as session:
         db_user = await UserDAO.get_user_by_name(session, form_data.username)
     if not db_user:
         raise HTTPException(status.HTTP_401_UNAUTHORIZED, detail="User not found")
     if not db_user.password == form_data.password:
         raise HTTPException(status.HTTP_403_FORBIDDEN, detail="Incorrect password")
-    return {"access_token": db_user.name, "token_type": "bearer"}
+    # delegate token creation partly to make testing easier
+    return create_token(db_user.name)
+
+
+def create_token(username: str):
+    "Create a JWT token for the given username"
+    encoded_key = jwt.encode({"user": username}, SECRET_KEY, algorithm=ALGORITHM)
+    return {"access_token": encoded_key, "token_type": "bearer"}
 
 
 async def get_user(token: str = Depends(oauth2_scheme)):
+    "Return the user for the given JWT token"
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("user")
+        if username is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
     async with db_session() as session:
-        db_user = await UserDAO.get_user_by_name(session, token)
-    if not db_user:
-        raise HTTPException(status.HTTP_401_UNAUTHORIZED, detail="Invalid token/user")
-    return db_user
+        user = await UserDAO.get_user_by_name(session, username)
+        if user is None:
+            raise credentials_exception
+    return user
