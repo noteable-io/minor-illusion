968097b54341a3b30f4262d19beac3eb303c87a3
Kafonek <matt.kafonek@noteable.io>
2022-01-10 06:51:59 -0500

    /me and /host endpoints into new router

---
+++ backend/src/app/debug.py
@@ -0,0 +1,21 @@
+import os
+
+from fastapi import APIRouter, Depends
+
+from app.auth import get_user
+from app.models import UserDAO
+from app.schemas import UserOut
+
+router = APIRouter()
+
+
+@router.get("/me", response_model=UserOut)
+def me(user: UserDAO = Depends(get_user)):
+    return user
+
+
+# Useful for seeing which backend your browser is connected to
+# when multiple backends are running behind load-balancer.
+@router.get("/host")
+def environ():
+    return os.environ.get("HOSTNAME")
