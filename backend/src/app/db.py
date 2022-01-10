f47a0e050aedd2b6fd50276c5ac29f8aa2242802
Kafonek <matt.kafonek@noteable.io>
2022-01-10 07:19:58 -0500

    explanatory comment

--- backend/src/app/db.py
+++ backend/src/app/db.py
@@ -8,6 +8,9 @@ from app.settings import get_settings
 
 @asynccontextmanager
 async def db_session():
+    # Putting engine creation and sessionmaker in here
+    # so that importing app.db doesn't trigger get_settings().
+    # TODO: determine best syntax to use for engine/sessionmaker
     engine = create_async_engine(get_settings().DB_DSN)
     async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
     session = async_session()
