3b3704d162768a144c6f7bbbec30c025d449d1da
Kafonek <matt.kafonek@noteable.io>
2022-01-10 06:51:20 -0500

    move engine/sessionmaker into function so that get_settings() is not triggered on module import

--- backend/src/app/db.py
+++ backend/src/app/db.py
@@ -5,12 +5,11 @@ from sqlalchemy.orm import sessionmaker
 
 from app.settings import get_settings
 
-engine = create_async_engine(get_settings().DB_DSN)
-async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
-
 
 @asynccontextmanager
 async def db_session():
+    engine = create_async_engine(get_settings().DB_DSN)
+    async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
     session = async_session()
     try:
         yield session
