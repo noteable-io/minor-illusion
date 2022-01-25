af5f5deb7d6b31922dd1c4470fcf28e6a7ffa8f1
Rohit Sanjay <rohit.sanjay@noteable.io>
2022-01-25 12:35:49 -0800

    Define seed data migration

--- backend/src/migrations/versions/0.2.1_org_space_user_todo_seed_data.py
+++ backend/src/migrations/versions/0.2.1_org_space_user_todo_seed_data.py
@@ -1,8 +1,8 @@
 """Create seed data for users and todos
 
-Revision ID: 0.1.1
-Revises: 0.1.0
-Create Date: 2021-11-30 21:02:13.637947
+Revision ID: 0.2.1
+Revises: 0.2.0
+Create Date: 2022-01-25 11:55:15.740915
 
 """
 import uuid
@@ -13,8 +13,8 @@ from alembic import op
 from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
-revision = "0.1.1"
-down_revision = "0.1.0"
+revision = "0.2.1"
+down_revision = "0.2.0"
 branch_labels = None
 depends_on = None
 
@@ -31,6 +31,29 @@ def upgrade():
 
     # table structure copied from 0.1.0 migration
     # TODO: remove this and use reflection once bug is fixed
+    org_table = sa.Table(
+        "organizations",
+        sa.MetaData(),
+        sa.Column("id", postgresql.UUID(as_uuid=True), nullable=False),
+        sa.Column("created_at", sa.TIMESTAMP(timezone=True), nullable=False),
+        sa.Column("name", sa.String(), nullable=True),
+        sa.PrimaryKeyConstraint("id"),
+    )
+
+    space_table = sa.Table(
+        "spaces",
+        sa.MetaData(),
+        sa.Column("id", postgresql.UUID(as_uuid=True), nullable=False),
+        sa.Column("created_at", sa.TIMESTAMP(timezone=True), nullable=False),
+        sa.Column("name", sa.String(), nullable=True),
+        sa.Column("organization_id", postgresql.UUID(as_uuid=True), nullable=True),
+        sa.ForeignKeyConstraint(
+            ["organization_id"],
+            ["organizations.id"],
+        ),
+        sa.PrimaryKeyConstraint("id"),
+    )
+
     user_table = sa.Table(
         "users",
         sa.MetaData(),
@@ -42,6 +65,11 @@ def upgrade():
         ),
         sa.Column("name", sa.String(), nullable=True),
         sa.Column("password", sa.String(), nullable=True),
+        sa.Column("organization_id", postgresql.UUID(as_uuid=True)),
+        sa.ForeignKeyConstraint(
+            ["organization_id"],
+            ["organizations.id"]
+        ),
         sa.PrimaryKeyConstraint("id"),
     )
 
@@ -57,11 +85,56 @@ def upgrade():
         sa.Column("title", sa.String(), nullable=True),
         sa.Column("content", sa.String(), nullable=True),
         sa.Column("user_id", postgresql.UUID(as_uuid=True), nullable=True),
+        sa.Column("space_id", postgresql.UUID(as_uuid=True)),
         sa.ForeignKeyConstraint(
             ["user_id"],
             ["users.id"],
         ),
+        sa.ForeignKeyConstraint(
+            ["space_id"],
+            ["spaces.id"]
+        ),
         sa.PrimaryKeyConstraint("id"),
+    )
+
+    org1_uuid = uuid.uuid4()
+    org2_uuid = uuid.uuid4()
+    op.bulk_insert(
+        org_table,
+        [
+            {"id": org1_uuid, "created_at": datetime.now(timezone.utc), "name": "org1"},
+            {"id": org2_uuid, "created_at": datetime.now(timezone.utc), "name": "org2"},
+            {
+                "id": uuid.uuid4(),
+                "created_at": datetime.now(timezone.utc),
+                "name": "org3",
+            },
+        ],
+    )
+
+    space1_uuid = uuid.uuid4()
+    op.bulk_insert(
+        space_table,
+        [
+            {
+                "id": space1_uuid,
+                "created_at": datetime.now(timezone.utc),
+                "name": "space1",
+                "organization_id": org1_uuid,
+            },
+            {
+                "id": uuid.uuid4(),
+                "created_at": datetime.now(timezone.utc),
+                "name": "space2",
+                "organization_id": org2_uuid,
+            },
+            {
+                "id": uuid.uuid4(),
+                "created_at": datetime.now(timezone.utc),
+                "name": "space3",
+                "organization_id": org1_uuid,
+            },
+        ],
     )
 
     user1_uuid = uuid.uuid4()  # <- need to save this for Todo creation
@@ -73,18 +146,21 @@ def upgrade():
                 "created_at": datetime.now(timezone.utc),
                 "name": "user1",
                 "password": "pass",
+                "organization_id": org1_uuid,
             },
             {
                 "id": uuid.uuid4(),
                 "created_at": datetime.now(timezone.utc),
                 "name": "user2",
                 "password": "pass",
+                "organization_id": org1_uuid,
             },
             {
                 "id": uuid.uuid4(),
                 "created_at": datetime.now(timezone.utc),
                 "name": "user3",
                 "password": "pass",
+                "organization_id": org2_uuid,
             },
         ],
     )
@@ -98,6 +174,7 @@ def upgrade():
                 "title": "Note 1",
                 "content": "My first Note",
                 "user_id": user1_uuid,
+                "space_id": space1_uuid,
             },
             {
                 "id": uuid.uuid4(),
@@ -105,6 +182,7 @@ def upgrade():
                 "title": "Note 2",
                 "content": "Edit this Note",
                 "user_id": user1_uuid,
+                "space_id": space1_uuid,
             },
             {
                 "id": uuid.uuid4(),
@@ -112,6 +190,7 @@ def upgrade():
                 "title": "Note 3",
                 "content": "Delete this Note",
                 "user_id": user1_uuid,
+                "space_id": space1_uuid,
             },
         ],
     )
