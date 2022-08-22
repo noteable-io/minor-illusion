29d1858eba3cd4a5fad9938e9b2d30d42e79c1f0
Kafonek <matt.kafonek@noteable.io>
2022-08-22 06:27:35 -0400

    Fix /api/docs behind reverse proxy

--- backend/src/app/main.py
+++ backend/src/app/main.py
@@ -1,10 +1,10 @@
-from fastapi import FastAPI
+import os
 
 from app.auth import router as AuthRouter
 from app.crud import router as CrudRouter
 from app.debug import router as DebugRouter
 from app.log_utils import setup_logging
-from app.settings import get_settings
+from fastapi import FastAPI
 
 setup_logging()
 
@@ -12,8 +12,10 @@ setup_logging()
 def build_app() -> FastAPI:
     "Builds the minor-illusion FastAPI application"
     # this is encapsulated in a function to facilitate testing
-    settings = get_settings()
-    app = FastAPI(root_path=settings.ROOT_PATH)
+    if "ROOT_PATH" in os.environ:
+        app = FastAPI(root_path=os.environ["ROOT_PATH"])
+    else:
+        app = FastAPI()
     app.include_router(AuthRouter)
     app.include_router(CrudRouter)
     app.include_router(DebugRouter)
